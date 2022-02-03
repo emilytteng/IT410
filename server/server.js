af8a926b81a574273fedb040d6714f116ade6174
emilytteng <etteng@byu.edu>
2022-02-02 18:21:23 -0700

    Express test GET and POST

--- server/server.js
+++ server/server.js
@@ -1,30 +1,33 @@
 const express = require('express')
-const { contentType } = require('express/lib/response')
 const app = express()
 const port = 3000
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
 
 // Route Definition Structure: 
 // app.METHOD(PATH, HANDLER)
 
-//app.use(express.json())
+app.use(express.json())
+app.use(express.text())
 
 app.get('/', (req, res) => {
+    console.log('GET Hello World')
     res.send('Hello World!')
 })
 
-// accept only "text/plain" or "application/json"
-// if either, return body type
-// if not return 400 response status
 app.post('/', (req, res) => {
-    //if req type
-    console.log('Body type: ', express.json(contentType))
-    res.send('Ok')
-    
-    //else
-    //console.log()
-    //res.send() 400
+    if (req.is('text/plain')) {
+        console.log('Text POST Request')
+        res.send({Content: 'text/plain', Body: req.body})
+    }
+    else if (req.is('application/json')) {
+        console.log('Json POST Request')
+        res.send({Content: 'application/json', Body: req.body})
+    }
+    else {
+        console.log('Request Body not Json or Text')
+        res.status(400).send('Content type not text/plain or application/json')
+    }
 })
