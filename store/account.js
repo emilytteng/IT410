99386f1ea6ba3eba1ac8a2c00d590a5e4f2b153e
emilytteng <etteng@byu.edu>
2022-03-14 18:57:25 -0600

    vuex authentication

---
+++ store/account.js
@@ -0,0 +1,37 @@
+export const state = () => {
+    return {
+        user: getUserFromCookie()
+    }
+}
+
+export const mutations = {
+    setUser (state, user) {
+        state.user = user
+    }
+}
+
+export const actions = {
+    async login ({ commit }, { username, password }) {
+        const res = await this.$axios.put('/api/authentication/login', {
+            username,
+            password
+        })
+        if (res.status === 200) {
+            commit('setUser', getUserFromCookie())
+        }
+    },
+
+    async logout ({ commit }) {
+        const res = await this.$axios.put('/api/authentication/logout')
+        if (res.status === 200) {
+            commit('setUser', null)
+        }
+    }
+}
+
+function getUserFromCookie () {
+    // Check if the user cookie is set and if so get the cookie value
+    const re = new RegExp("user=([^;]+)") 
+    const value = re.exec(document.cookie)
+    return value != null ? unescape(value[1]) : null
+}
