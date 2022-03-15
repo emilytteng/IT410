1ad575d50269d88f8d77254e35b5c8e586f98b8b
emilytteng <etteng@byu.edu>
2022-03-14 18:56:32 -0600

    set cookie on browser for javascript

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -6,6 +6,11 @@ module.exports = function (passport) {
             console.log("login reached")
             authenticate(req, res, err => {
                 if (err) return next(err)
+                // tell browser to set an extra cookie
+                // cookie not secure, but can help the UI determine if user is logged in or not.
+                res.cookie('user', req.user.username, {
+                    maxAge: 36000000 // expires in 10 hours
+                })
                 res.enforcer.status(200).send()
                 console.log("auth success")
             })
@@ -15,6 +20,7 @@ module.exports = function (passport) {
         logout (req, res) {
             console.log("logout reached")
             if (req.user) req.logout()
+            res.clearCookie("user")
             res.enforcer.status(200).send()
             console.log("logout success")
         }
